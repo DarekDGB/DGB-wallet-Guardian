5b1bff4ee19e6774283ad82fcc051dd96d56d50f
DarekDGB <majescyk@gmail.com>
2025-11-24 14:14:58 +0000

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,3 +1,3 @@
 def test_imports():
-    import dgb_wallet_guardian.guardian_core
-    import dgb_wallet_guardian.guardian_rules
+    import dgb_wallet_guardian
+    from dgb_wallet_guardian import client, config, guardian_engine, models
